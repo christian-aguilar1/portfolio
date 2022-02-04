3cf9c6a524e43bc665466365972eb7af2c0414af
christian-aguilar1 <christianaguilarm1@gmail.com>
2022-02-04 00:29:30 -0300

    improved contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { MailSenderService } from '../services/mail-sender.service';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -11,9 +13,10 @@ export class ContactComponent implements OnInit {
 
   form!: FormGroup;
   submitted = false;
+  clicked = false;
   isLoading!: boolean;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private mailSender: MailSenderService) {
     this.buildForm();
   }
 
@@ -22,25 +25,21 @@ export class ContactComponent implements OnInit {
 
   sendEmail(event: Event) {
     event.preventDefault();
-    console.log("ala")
+    this.clicked = true;
+
     if (this.form.valid) {
       this.submitted = true;
-      this.form.disable();
-      var formData: any = new FormData();
-      const value = this.form.value;
-      let email = value.email;
-      let text = value.message;
-      formData.append("name", email);
-      formData.append("email", email);
-      formData.append("message", text);
-      console.log(formData);
-      this.submitted = false;
       this.isLoading = true;
+      this.form.disable();
+      const value = this.form.value;
+      this.mailSender.sendMessage(value.name, value.email, value.message);
+      this.isLoading = false;
     }
   }
 
   private buildForm() {
     this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(8)]],
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required, Validators.minLength(10)]],
     });
